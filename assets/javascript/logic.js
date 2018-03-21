c398c787c6b681bf12683f03ddcea4c084afd82e
SigmaVX <tonywible@gmail.com>
2018-03-20 23:20:19 -0400

    first draft of card animations put in place

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -89,11 +89,12 @@ function makeMoment(i){
         newSection.append(newContainer);
 	
 	// Row 1
-	var newRowOne = $("<div>").attr("class", "row no-gutters text-center");
+	var newRowOne = $("<div>").attr("class", "row no-gutters text-center align-items-end");
         newContainer.append(newRowOne);
 		
 		// Div 1
-		var movieDivOne = $("<div>").attr("class", "col-2");
+		var movieDivOne = $("<div>").attr("class", "col-2 card animated flipInX");
+			movieDivOne.attr("style", "animation-delay: 1s");
 			newRowOne.append(movieDivOne);
 			var moviePosterOne = $("<img>").attr({
 					class: "img-fluid",
@@ -103,24 +104,26 @@ function makeMoment(i){
 					movieDivOne.append(moviePosterOne);
 		
 		// Div 2 
-		var newsCardOne = $("<div>").attr("class", "card col-2 news-card");
+		var newsCardOne = $("<div>").attr("class", "card col-2 news-card animated flipInY");
+			newsCardOne.attr("style", "animation-delay: 0.2s")		
 			newRowOne.append(newsCardOne);
-				var cardImgOne = $("<img>").attr({
+				var newsImgOne = $("<img>").attr({
 					class: "card-img-top img-fluid",
 					alt: "news-image",
 					src: "./assets/images/test-news.jpg"
 					});
-					newsCardOne.append(cardImgOne);
+					newsCardOne.append(newsImgOne);
 				
-				var cardBodyOne = $("<div>").attr("class", "card-body text-center");
-					newsCardOne.append(cardBodyOne);
+				var newsBodyOne = $("<div>").attr("class", "card-body text-center");
+					newsCardOne.append(newsBodyOne);
 
 					var headlineOne = $("<h5>").html("Hawks Rally Late to Whittle Away at Knicks’ Lead");
 						headlineOne.attr("class", "card-title headline-text" );
-						cardBodyOne.append(headlineOne);
+						newsBodyOne.append(headlineOne);
 
 		// Div 3
-		var tvDivOne = $("<div>").attr("class", "col-2");
+		var tvDivOne = $("<div>").attr("class", "card col-2 animated flipInX");
+			tvDivOne.attr("style", "animation-delay: 0.6s")
 			newRowOne.append(tvDivOne);
 			var tvPosterOne = $("<img>").attr({
 					class: "img-fluid",
@@ -130,7 +133,8 @@ function makeMoment(i){
 					tvDivOne.append(tvPosterOne);
 
 		// Div 4
-		var movieDivTwo = $("<div>").attr("class", "col-2");
+		var movieDivTwo = $("<div>").attr("class", "card col-2 animated flipInY");
+			movieDivTwo.attr("style", "animation-delay: 1.2s");
 			newRowOne.append(movieDivTwo);
 			var moviePosterTwo = $("<img>").attr({
 					class: "img-fluid",
@@ -140,7 +144,8 @@ function makeMoment(i){
 					movieDivTwo.append(moviePosterTwo);
 
 		// Div 5
-		var musicCardOne = $("<div>").attr("class", "card col-2 music-card");
+		var musicCardOne = $("<div>").attr("class", "card col-2 music-card animated flipInX");
+			musicCardOne.attr("style", "animation-delay: 0.4s")
 			newRowOne.append(musicCardOne);
 				var musicImgOne = $("<img>").attr({
 					class: "card-img-top img-fluid",
@@ -154,11 +159,12 @@ function makeMoment(i){
 
 					var musicHeadlineOne = $("<h5>").html("Hawks Rally Late to Whittle Away at Knicks’ Lead");
 						musicHeadlineOne.attr("class", "card-title headline-text" );
-						musicCardOne.append(musicHeadlineOne);
+						musicBodyOne.append(musicHeadlineOne);
 
 
 		// Div 6
-		var tvDivTwo = $("<div>").attr("class", "col-2");
+		var tvDivTwo = $("<div>").attr("class", "card col-2 animated flipInY");
+			tvDivTwo.attr("style", "animation-delay: 0.8s")
 			newRowOne.append(tvDivTwo);
 			var tvPosterTwo = $("<img>").attr({
 					class: "img-fluid",
@@ -168,11 +174,12 @@ function makeMoment(i){
 					tvDivTwo.append(tvPosterTwo);
 	
 	// Row 2
-	var newRowTwo = $("<div>").attr("class", "row no-gutters text-center");
+	var newRowTwo = $("<div>").attr("class", "row no-gutters text-center align-items-start");
 	newContainer.append(newRowTwo);
 		
 		// Div 1
-		var musicCardTwo = $("<div>").attr("class", "card col-2 music-card");
+		var musicCardTwo = $("<div>").attr("class", "card col-2 music-card animated flipInY");
+			musicCardTwo.attr("style", "animation-delay: 0.1s");
 			newRowTwo.append(musicCardTwo);
 				var musicImgTwo = $("<img>").attr({
 					class: "card-img-top img-fluid",
@@ -186,10 +193,11 @@ function makeMoment(i){
 
 					var musicHeadlineTwo = $("<h5>").html("Hawks Rally Late to Whittle Away at Knicks’ Lead");
 						musicHeadlineTwo.attr("class", "card-title headline-text" );
-						musicCardTwo.append(musicHeadlineTwo);
+						musicBodyTwo.append(musicHeadlineTwo);
 
 		// Div 2
-		var tvDivThree = $("<div>").attr("class", "col-2");
+		var tvDivThree = $("<div>").attr("class", "card col-2 animated flipInX");
+			tvDivThree.attr("style", "animation-delay: 0.9s");
 			newRowTwo.append(tvDivThree);
 			var tvPosterThree = $("<img>").attr({
 				class: "img-fluid",
@@ -199,24 +207,26 @@ function makeMoment(i){
 				tvDivThree.append(tvPosterThree);
 
 		// Div 3
-		var newsCardTwo = $("<div>").attr("class", "card col-2 news-card");
-		newRowTwo.append(newsCardTwo);
-			var cardImgTwo = $("<img>").attr({
-				class: "card-img-top img-fluid",
-				alt: "news-image",
-				src: "./assets/images/test-news.jpg"
-				});
-				newsCardTwo.append(cardImgTwo);
-			
-			var newsBodyTwo = $("<div>").attr("class", "card-body text-center");
-				newsCardTwo.append(newsBodyTwo);
+		var newsCardTwo = $("<div>").attr("class", "card col-2 news-card animated flipInY");
+			newsCardTwo.attr("style", "animation-delay: 0.3s")
+			newRowTwo.append(newsCardTwo);
+				var cardImgTwo = $("<img>").attr({
+					class: "card-img-top img-fluid",
+					alt: "news-image",
+					src: "./assets/images/test-news.jpg"
+					});
+					newsCardTwo.append(cardImgTwo);
+				
+				var newsBodyTwo = $("<div>").attr("class", "card-body text-center");
+					newsCardTwo.append(newsBodyTwo);
 
-				var headlineTwo = $("<h5>").html("Hawks Rally Late to Whittle Away at Knicks’ Lead");
-					headlineTwo.attr("class", "card-title headline-text" );
-					newsBodyTwo.append(headlineTwo);
+					var headlineTwo = $("<h5>").html("Hawks Rally Late to Whittle Away at Knicks’ Lead");
+						headlineTwo.attr("class", "card-title headline-text" );
+						newsBodyTwo.append(headlineTwo);
 
 		// Div 4
-		var musicCardThree = $("<div>").attr("class", "card col-2 music-card");
+		var musicCardThree = $("<div>").attr("class", "card col-2 music-card animated flipInX");
+			musicCardThree.attr("style", "animation-delay: 0.7s");
 			newRowTwo.append(musicCardThree);
 				var musicImgThree = $("<img>").attr({
 					class: "card-img-top img-fluid",
@@ -230,10 +240,11 @@ function makeMoment(i){
 
 					var musicHeadlineThree = $("<h5>").html("Hawks Rally Late to Whittle Away at Knicks’ Lead");
 						musicHeadlineThree.attr("class", "card-title headline-text" );
-						musicCardThree.append(musicHeadlineThree);
+						musicBodyThree.append(musicHeadlineThree);
 			
 		// Div 5
-		var movieDivThree = $("<div>").attr("class", "col-2");
+		var movieDivThree = $("<div>").attr("class", "card col-2 animated flipInY");
+			movieDivThree.attr("style", "animation-delay: 0.5s")
 			newRowTwo.append(movieDivThree);
 			var moviePosterThree = $("<img>").attr({
 					class: "img-fluid",
@@ -244,32 +255,34 @@ function makeMoment(i){
 
 		
 		// Div 6
-		var newsCardThree = $("<div>").attr("class", "card col-2 news-card");
-		newRowTwo.append(newsCardThree);
-			var cardImgThree = $("<img>").attr({
-				class: "card-img-top img-fluid",
-				alt: "news-image",
-				src: "./assets/images/test-news.jpg"
-				});
-				newsCardThree.append(cardImgThree);
-			
-			var newsBodyThree = $("<div>").attr("class", "card-body text-center");
-				newsCardThree.append(newsBodyThree);
+		var newsCardThree = $("<div>").attr("class", "card col-2 news-card animated flipInX");
+			newsCardThree.attr("style", "animation-delay: 1.1s");	
+			newRowTwo.append(newsCardThree);
+				var cardImgThree = $("<img>").attr({
+					class: "card-img-top img-fluid",
+					alt: "news-image",
+					src: "./assets/images/test-news.jpg"
+					});
+					newsCardThree.append(cardImgThree);
+				
+				var newsBodyThree = $("<div>").attr("class", "card-body text-center"); 
+					newsCardThree.append(newsBodyThree);
 
-				var headlineThree = $("<h5>").html("Hawks Rally Late to Whittle Away at Knicks’ Lead");
-					headlineThree.attr("class", "card-title headline-text" );
-					newsBodyThree.append(headlineThree);
-			
+					var headlineThree = $("<h5>").html("Hawks Rally Late to Whittle Away at Knicks’ Lead");
+						headlineThree.attr("class", "card-title headline-text" );
+						newsBodyThree.append(headlineThree);
+				
 	init();
 
 }
 
-            
+
 
 	// Runs the JQ to Display Images and Start Annimation 
 	$("#start").on("click", function(event){
 		event.preventDefault();
 		console.log("click OK");
+		makeMoment();
 		makeMoment();
 		$("#start-section").hide();
 	});
